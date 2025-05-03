d1180d2b3799646503e4018a5682007ab6ccf01b
MattToegel <matthew.toegel@njit.edu>
2025-05-02 20:26:44 -0400

    added make.sh file

---
+++ make.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+cd "$1" || exit 1
+
+# Compile all source files
+find . -name "*.java" > sources.txt
+javac @sources.txt
+
+cd ..
+project="${1%/}" # remove trailing slash
+mkdir -p "$project/build"
+
+# Create Server JAR (include Server and Common packages)
+jar cfe "$project/build/${project}_Server.jar" "${project}.Server.Server" \
+  $project/Server/*.class $project/Common/*.class $project/Exceptions/*.class
+
+# Create Client JAR (include Client and Common packages)
+jar cfe "$project/build/${project}_Client.jar" "${project}.Client.ClientUI" \
+  $(find "$project/Client" -name "*.class") $(find "$project/Common" -name "*.class")
+
+# Create run.bat for Windows users
+{ 
+  echo "java -jar ${project}_Client.jar"
+  echo "PAUSE"
+} > "$project/build/run.bat"
