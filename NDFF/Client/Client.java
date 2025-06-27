9ee088c6085685be8e5b5a1ce8ba53b1583b7515
MattToegel <matthew.toegel@njit.edu>
2025-06-27 19:25:51 -0400

    added missing catch

--- NDFF/Client/Client.java
+++ NDFF/Client/Client.java
@@ -414,6 +414,8 @@ public enum Client {
                 LoggerUtil.INSTANCE.warning("Connection dropped");
                 e.printStackTrace();
             }
+        } catch (Exception e) {
+            LoggerUtil.INSTANCE.severe("Unexpected error in listenToServer", e);
         } finally {
             closeServerConnection();
         }
